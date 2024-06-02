2c4c4664e3b4c52eb1558052d5e4c132aa4de7b4
GenWatt <adrian.kosoglos@gmail.com>
2024-06-02 21:01:02 +0200

    Setup base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// @ts-ignore
+import { ConfigEnv, defineConfig, loadEnv } from 'vite';
+
+
+export default ({ mode }: ConfigEnv) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    assetsInclude: ['**/*.glb', '**/*.gltf'],
+    // @ts-ignore
+    base: mode === 'production' ? '/Portfolio-ubb/' : '/',
+  })
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ['**/*.glb', '**/*.gltf'],
-})
