6bc6d360e5aa22b1ab4ba3d493c9ce09513c920d
GenWatt <adrian.kosoglos@gmail.com>
2025-02-09 21:11:56 +0100

    change workersrc for pdf reader

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,7 @@ import { SnackbarProvider } from 'notistack'
 import { LanguageProvider } from './context/LanguageContext'
 import { pdfjs } from 'react-pdf';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const isProduction = import.meta.env.MODE === 'production'
 const basename = isProduction ? '/Portfolio-ubb/' : '/'
